ee3644af1b758cc04a6fb0fdc020e1068ee853fb
sofiaq2201 <sofiaq2201@gmail.com>
2020-08-11 23:47:03 -0300

    Added dependecies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarComponent } from './blocknotas/listar/listar.component';
+import { AddComponent } from './blocknotas/add/add.component';
+import { EditComponent } from './blocknotas/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listar', component: ListarComponent },
+  { path: 'add', component: AddComponent },
+  { path: 'edit', component: EditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bloc-notas-angular';
+  
+  constructor(private router: Router) { }
+
+  listar() {
+    this.router.navigate(['listar']);
+  }
+
+  add() {
+    this.router.navigate(['add']);
+  }
 }
